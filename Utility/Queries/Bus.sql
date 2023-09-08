976b887ef9645143e9e6598c3ec3963512d0d410
SaltedFishy03 <seekerbrutus@gmail.com>
2023-09-08 13:34:31 +0200

    Bus FileQuery done

---
+++ Utility/Queries/Bus.sql
@@ -0,0 +1,16 @@
+USE Auction
+GO
+
+-- Create the Bus table if it doesn't exist.
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Bus')
+BEGIN
+    CREATE TABLE Bus
+    (
+        Id INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+        NumberOfSeats TINYINT NOT NULL,
+        NumberOfWheels TINYINT NOT NULL,
+        HasToilet BIT NOT NULL,
+        HeavyVehicleId INT NOT NULL FOREIGN KEY REFERENCES HeavyVehicle(Id)
+    )
+END
+GO
