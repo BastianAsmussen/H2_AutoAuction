ae389d4f293d280743565ab247624c8a842bac98
Bastian Asmussen <bastianasmussen@gmail.com>
2023-09-08 13:21:30 +0200

    Moved FuelType to its own file.

--- Utility/Queries/Vehicle.sql
+++ Utility/Queries/Vehicle.sql
@@ -1,17 +1,6 @@
 USE Auction
 GO
 
--- Create the FuelType table if it does not exist.
-IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'FuelType')
-BEGIN
-    CREATE TABLE FuelType
-    (
-        Id TINYINT IDENTITY(1,1) PRIMARY KEY,
-        Type varchar(2) NOT NULL,
-    )
-END
-GO
-
 -- Create the EnergyClass table if it does not exist.
 IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'EnergyClass')
 BEGIN
