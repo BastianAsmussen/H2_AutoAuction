9e7292b320ee12405cc4554acf8f3861a8df3d51
SaltedFishy03 <seekerbrutus@gmail.com>
2023-09-08 13:08:33 +0200

    Moved PrivatePersonalCar to seperate file

--- Utility/Queries/PersonalCar.sql
+++ Utility/Queries/PersonalCar.sql
@@ -27,15 +27,5 @@ BEGIN
 END
 GO 
 
--- Create the PrivatePersonalCar table if it doesn't exist
-IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'PrivatePersonalCar')
-BEGIN
-    CREATE TABLE PrivatePersonalCar
-    (
-        Id INT IDENTITY(1,1) PRIMARY KEY,
-        HasIsofixFittings BIT NOT NULL,
-        CarId INT NOT NULL FOREIGN KEY REFERENCES Car(Id),
-    )
-END
-GO
+
 
