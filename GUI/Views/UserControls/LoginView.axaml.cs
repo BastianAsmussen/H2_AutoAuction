d7f4582e9b0215bd5278e4654a0cf901c5b4edbb
Kevin Bamwesa Ishara <kevi3758@gmail.com>
2023-09-11 13:52:07 +0200

    Working on the error if the fields are empty

---
+++ GUI/Views/UserControls/LoginView.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace GUI.Views.UserControls;
+
+public partial class LoginView : UserControl
+{
+    public LoginView()
+    {
+        InitializeComponent();
+    }
+}
