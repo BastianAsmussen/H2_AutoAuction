6c08608fc76c91f77b1b82fd790c17ba312a869b
Bastian Asmussen <bastianasmussen@gmail.com>
2023-09-11 13:11:14 +0200

    Added LoadCapacity.

--- Utility/Queries/Trucks.sql
+++ Utility/Queries/Trucks.sql
@@ -1,12 +1,14 @@
 USE Auction
 GO
 
--- Create the Truck table if it doesn't exist.
+-- Create the Trucks table if it doesn't exist.
 IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Trucks')
    BEGIN
        CREATE TABLE Trucks
        (
            Id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+
+           LoadCapacity FLOAT NOT NULL,
 
            HeavyVehicleId INT NOT NULL FOREIGN KEY REFERENCES HeavyVehicles(Id),
        )
