a894fa3255b47ed5be9e29b1d916b4e012cbd41b
Kevin Bamwesa Ishara <kevi3758@gmail.com>
2023-09-13 09:39:04 +0200

    Signup UC created

---
+++ GUI/ViewModels/CreateUserViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ReactiveUI;
+
+namespace GUI.ViewModels;
+
+public class CreateUserViewModel : ViewModelBase
+{
+    
+    #region Properties
+    private string _userName;
+    private string _passWord;
+    private string _rPassWord;
+    private bool _btnCreateEnabled;
+    private bool _btnCancelEnabled;
+   
+    [Required]
+    public string UserName
+    {
+        get => _userName;
+        set => this.RaiseAndSetIfChanged(ref _userName, value);
+    }
+
+    [Required]
+    public string PassWord
+    {
+        get => _passWord;
+        set => this.RaiseAndSetIfChanged(ref _passWord, value);
+    }
+    
+    [Required]
+    public string RPassWord
+    {
+        get => _rPassWord;
+        set => this.RaiseAndSetIfChanged(ref _rPassWord, value);
+    }
+
+    public bool BtnCreateEnabled
+    {
+        get => _btnCreateEnabled;
+        set => this.RaiseAndSetIfChanged(ref _btnCreateEnabled, value);
+    }
+
+    public bool BtnCancelEnabled
+    {
+        get => _btnCancelEnabled;
+        set => this.RaiseAndSetIfChanged(ref _btnCancelEnabled, value);
+    }
+    #endregion
+    
+    public CreateUserViewModel()
+    {
+        UserName = string.Empty;
+        PassWord = string.Empty;
+        RPassWord = string.Empty;
+        BtnCreateEnabled = false;
+        BtnCancelEnabled = true;
+    }
+
+}
