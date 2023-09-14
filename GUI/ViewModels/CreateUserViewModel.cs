8f837af1bd6175dd00eb3301318924447e1b47ab
Kevin Bamwesa Ishara <kevi3758@gmail.com>
2023-09-14 08:21:30 +0200

    Created Create and Cancel Button

--- GUI/ViewModels/CreateUserViewModel.cs
+++ GUI/ViewModels/CreateUserViewModel.cs
@@ -1,18 +1,21 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Windows.Input;
+using GUI.Views.UserControls;
 using ReactiveUI;
 
 namespace GUI.ViewModels;
 
 public class CreateUserViewModel : ViewModelBase
 {
-    
     #region Properties
+
     private string _userName;
     private string _passWord;
     private string _rPassWord;
-    private bool _btnCreateEnabled;
-    private bool _btnCancelEnabled;
-   
+    private bool _isCorporate;
+    private bool _isPrivate;
+
     [Required]
     public string UserName
     {
@@ -26,7 +29,7 @@ public class CreateUserViewModel : ViewModelBase
         get => _passWord;
         set => this.RaiseAndSetIfChanged(ref _passWord, value);
     }
-    
+
     [Required]
     public string RPassWord
     {
@@ -34,26 +37,76 @@ public class CreateUserViewModel : ViewModelBase
         set => this.RaiseAndSetIfChanged(ref _rPassWord, value);
     }
 
-    public bool BtnCreateEnabled
+    public bool IsCorporate
     {
-        get => _btnCreateEnabled;
-        set => this.RaiseAndSetIfChanged(ref _btnCreateEnabled, value);
+        get => _isCorporate;
+        set => this.RaiseAndSetIfChanged(ref _isCorporate, value);
     }
 
-    public bool BtnCancelEnabled
+    public bool IsPrivate
     {
-        get => _btnCancelEnabled;
-        set => this.RaiseAndSetIfChanged(ref _btnCancelEnabled, value);
+        get => _isPrivate;
+        set => this.RaiseAndSetIfChanged(ref _isPrivate, value);
     }
+
     #endregion
-    
+
+    public ICommand CancelCommand { get; }
+    public ICommand CreateCommand { get; }
+
+
     public CreateUserViewModel()
     {
+        CancelCommand = ReactiveCommand.Create(Cancel);
+        CreateCommand = ReactiveCommand.Create(Create);
+
+
         UserName = string.Empty;
         PassWord = string.Empty;
         RPassWord = string.Empty;
-        BtnCreateEnabled = false;
-        BtnCancelEnabled = true;
     }
 
+    void InputValidator()
+    {
+        // Validate input Email and Password, to enable Create Button
+    }
+
+    private void Create()
+    {
+        Utilities.ContentArea.Navigate(new LoginView());
+    }
+
+    private void Cancel()
+    {
+        Utilities.ContentArea.Navigate(new LoginView());
+    }
+
+    private void CreateUser()
+    {
+        if (PassWord.Equals(RPassWord))
+        {
+            // Send request to the server
+            try
+            {
+                UserCreationRequest(UserName, PassWord);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+
+    private void UserCreationRequest(string username, string password)
+    {
+        try
+        {
+            Console.WriteLine("User created");
+        }
+        catch (Exception EX_NAME)
+        {
+            throw new Exception("User not created");
+        }
+    }
 }
