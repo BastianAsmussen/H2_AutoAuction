ababe6116f6787669cae0563a0fe7a8372acb6ab
Kevin Bamwesa Ishara <kevi3758@gmail.com>
2023-09-14 17:03:31 +0200

    Every button works, and Datagrids shows data
    ! Binding to the correct data is missing

---
+++ GUI/Views/UserControls/BidHistoryView.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace GUI.Views.UserControls;
+
+public partial class BidHistoryView : UserControl
+{
+    public BidHistoryView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ GUI/Views/UserControls/SetForSaleView.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace GUI.Views.UserControls;
+
+public partial class SetForSaleView : UserControl
+{
+    public SetForSaleView()
+    {
+        InitializeComponent();
+    }
+}
