1255af12beb56ac762448c839b78d12ea270853f
Bastian Asmussen <bastianasmussen@gmail.com>
2023-09-15 10:03:27 +0200

    HOTFIX: Fixed minor typo in Users/Database.sql

--- Utility/Queries/Users/Database.sql
+++ Utility/Queries/Users/Database.sql
@@ -1,4 +1,4 @@
--- Create the Vehicle database if it does not exist.
+-- Create the User database if it does not exist.
 IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'User')
 BEGIN
     CREATE DATABASE User
