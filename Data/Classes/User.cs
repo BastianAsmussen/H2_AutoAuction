90e78ea274f50cf92ddafb7323ec0ef1d1c19e83
SaltedFishy03 <seekerbrutus@gmail.com>
2023-09-26 11:19:09 +0200

    A3 done
    Setforsale method done (Also returns the id of the auction)
    
    PlaceBid method done: place's a bid and then notifies the seller

--- Data/Classes/User.cs
+++ Data/Classes/User.cs
@@ -1,4 +1,7 @@
-﻿using Data.Interfaces;
+﻿using Data.Classes.Auctions;
+using Data.Classes.Vehicles;
+using Data.Interfaces;
+using Utility.DatabaseManager;
 
 namespace Data.Classes;
 
@@ -42,5 +45,45 @@ public class User : IBuyer, ISeller
                $"PasswordHash: {PasswordHash}\n" +
                $"Zipcode: {Zipcode}\n" +
                $"Balance: {Balance}\n";
+    }
+
+    
+    
+    /// <summary>
+    /// Set's a vehicle for sale
+    /// </summary>
+    /// <param name="vehicle">property form Vehicle class</param>
+    /// <param name="Seller">property form User class</param>
+    /// <param name="minBid"></param>
+    /// <exception cref="ArgumentException">If it fails to create an auction</exception>
+    /// <returns>The id of the auction</returns>
+    public uint SetForSale(Vehicle vehicle, User Seller, decimal minBid)
+    {
+        Auction auction = new Auction(0, vehicle, Seller, null, minBid);
+        try
+        {
+            auction = DatabaseManager.CreateAuction(auction);
+        }
+        catch (ArgumentException e)
+        {
+            Console.WriteLine("Error in SetForSale: " + e.Message);
+            throw;
+        }
+        
+        return auction.AuctionId ;
+    }
+    
+    
+    /// <summary>
+    ///  Notifies the seller the when a bid is over minimum bid
+    /// </summary>
+    /// <param name="auction"></param>
+    /// <param name="newBid"></param>
+    /// <returns></returns>
+    public uint PlaceBid(Auction auction, decimal newBid)
+    {
+        auction.Seller.ReceiveBidNotification($"New bid of {newBid} on {auction.Vehicle}.");
+
+        return 0;
     }
 }
--- Data/Interfaces/IBuyer.cs
+++ Data/Interfaces/IBuyer.cs
@@ -1,8 +1,13 @@
-﻿namespace Data.Interfaces;
+﻿using Data.Classes;
+using Data.Classes.Auctions;
+using Data.Classes.Vehicles;
+
+namespace Data.Interfaces;
 
 public interface IBuyer
 {
     public uint UserId { get; set; }
 
     public void SubBalance(decimal amount);
+    uint PlaceBid(Auction auction, decimal newBid);
 }
