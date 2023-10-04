281a2857859e7e66850e2e55c8af030f015a9cf8
SaltedFishy03 <seekerbrutus@gmail.com>
2023-10-04 11:40:50 +0200

    ...

--- GUI/ViewModels/BidHistoryViewModel.cs
+++ GUI/ViewModels/BidHistoryViewModel.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
 using Data.Classes.Auctions;
 using Data.DatabaseManager;
 using GUI.Utilities;
+using GUI.Views.UserControls;
 using ReactiveUI;
 
 namespace GUI.ViewModels;
@@ -17,23 +19,45 @@ public class BidHistoryViewModel : ViewModelBase
         set => this.RaiseAndSetIfChanged(ref _bidHistory, value);
     }
 
+    private ObservableCollection<Bid> _userBids;
+
+    public ObservableCollection<Bid> UserBids
+    {
+        get => _userBids;
+        set => this.RaiseAndSetIfChanged(ref _userBids, value);
+    }
+
+    private int _bidCount = 0;
+    public string FinalPrice => DatabaseManager.GetBidById(BidHistory[_bidCount++].BidId).Auction.CurrentPrice.ToString();
+
+    //     _finalPrices;
+    // set => this.RaiseAndSetIfChanged(ref _finalPrices, value);
+    public ICommand CancelCommand { get; }
+
     public BidHistoryViewModel()
     {
+        CancelCommand = ReactiveCommand.Create(() => ContentArea.Navigate(new HomeScreenView()));
         try
         {
-            LoadBids();
+            GetYourBids();
         }
         catch (Exception e)
         {
             Console.WriteLine($"Error loading auctions: {e.Message}");
         }
     }
-    
-    
-    private void LoadBids()
+
+    private void GetYourBids()
     {
-            var allAuctions = DatabaseManager.GetBidsByUser(UserInstance.GetCurrentUser());
-            _bidHistory = new(allAuctions);
+        UserBids = new ObservableCollection<Bid>(DatabaseManager.GetBidsByUser(UserInstance.GetCurrentUser()));
     }
 
+
+    // private void LoadBids()
+    // {
+    //     // var curentAuctions = DatabaseManager.get(bidder);
+    //     // var auctionBid = DatabaseManager.GetBidsByAuction(curentAuctions);
+    //     var allAuctions = DatabaseManager.GetBidsByUser(UserInstance.GetCurrentUser());
+    //     _bidHistory = new(allAuctions);
+    // }
 }
